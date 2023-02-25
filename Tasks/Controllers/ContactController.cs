81f12405e06d94051aa765fa51a494b1d1362683
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-02-25 03:59:43 -0800

    add: index route for comtact comtroller

---
+++ Tasks/Controllers/ContactController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tasks.Controllers
+{
+	public class ContactController : Controller
+	{
+		public String Index()
+		{
+			return "First Contact on the List is Chioma";
+		}
+
+		
+	}
+}
+
