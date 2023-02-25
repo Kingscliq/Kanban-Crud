78440a7c1b74996045defd400838bcb4e38b10f5
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-02-25 06:38:49 -0800

    set up of todo creation page

---
+++ Tasks/Controllers/TodosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Tasks.Models;
+
+namespace Tasks.Controllers
+{
+	public class TodosController : Controller
+	{
+		TodosModel task = new TodosModel("Bingo", "This is a Dog");
+
+		public IActionResult Index()
+		{
+			return View(task);
+		}
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ Tasks/Models/TodosModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Tasks.Models
+{
+	public class TodosModel
+	{
+		public String title { get; set; }
+        public string description { get; set; }
+		public Boolean isCompleted { get; set; }
+
+
+        public TodosModel(string title,  string description)
+        {
+			this.description = description;
+			this.title = title;
+        }
+        public TodosModel(string title, string description, Boolean isCompleted)
+			:this(title, description)
+		{
+			this.isCompleted = isCompleted ;
+		}
+    }
+}
+
